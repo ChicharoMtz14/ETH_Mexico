4a841a54ad6e73d0bf91e4e4b06e04acf7722144
César Martínez <cesar14martinez2021@gmail.com>
2024-09-11 02:57:10 -0600

    Primer Draft

---
+++ Prueba_Web.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder = 'Templates')
+
+# Ruta para la pantalla de inicio
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Ruta para el botón "Sobre el proyecto"
+@app.route('/sobre')
+def sobre():
+    return render_template('sobre.html')
+
+# Ruta para generar nueva votación (solo la interfaz por ahora)
+@app.route('/nueva_votacion')
+def nueva_votacion():
+    return render_template('nueva_votacion.html')
+
+# Ruta para consultar resultados previos (solo la interfaz por ahora)
+@app.route('/resultados')
+def resultados():
+    return render_template('resultados.html')
+
+# Iniciar la aplicación
+if __name__ == "__main__":
+    app.run(debug=True)
